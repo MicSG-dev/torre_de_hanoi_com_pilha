23f0fcc9b623cbce645db2dfa84af9fa6534727d
Michel Galvão <michelsgalvao@gmail.com>
2022-10-20 18:52:21 -0300

    Atualização Interface usuário e correção de bugs

--- Torre.py
+++ Torre.py
@@ -2,15 +2,22 @@ class Torre:
     def __init__(self, qtd_discos):
         self._discos = []
         self._qtd_discos = qtd_discos
+        self._numero_movimentos = 0
 
         for item in range(qtd_discos, 0, -1):
             self._discos.append(0)
 
-    def empilha_disco(self, peso):
+    def inicia_torre_com_disco(self, peso):
         self._discos.insert(0,peso)
+
+    def desempilha_disco(self):
+        self._discos.pop()
 
     def get_tamanho(self):
         return self._qtd_discos
+
+    def get_numero_movimentos(self):
+        return self._numero_movimentos
 
     def get_first(self):
         first = self._discos[self.get_tamanho() - 1]
@@ -22,8 +29,6 @@ class Torre:
             else:
                 indice=indice-1
         return first
-
-
 
     def set_vazio_first_position_torre(self):
         indice = self.get_tamanho() - 1
@@ -41,10 +46,14 @@ class Torre:
             if indice== 0:
                 break
             indice-=1
-        self._discos.insert(indice, peso)
 
-    def desempilha_disco(self):
-        self._discos.pop()
+        if indice != 0 and self._discos[indice-1] <peso:
+            print("Movimentação inválida: Um disco maior nunca deve ficar por cima de um disco menor")
+            return False
+        else:
+            self._discos.insert(indice, peso)
+            self._numero_movimentos += 1
+            return True
 
     def get_discos(self):
         return self._discos
--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from itertools import product
-
 from Torre import Torre
+
+def calcular_pontuacao(min_tentativas, num_tentativas):
+    pontuacao = 100 + ((min_tentativas-num_tentativas)/num_tentativas) * 100
+    return pontuacao
+
+def numero_tentativas_min(qtd_discos):
+    return 2 ** qtd_discos - 1
+
+def numero_tentativas():
+    return torre1.get_numero_movimentos()+torre2.get_numero_movimentos()+torre3.get_numero_movimentos()
 
 def qtd_max_discos():
     return 10
 
 def print_torres():
+    print("Estatísticas ➜ NÚMERO DE MOVIMENTOS: "+str(torre1.get_numero_movimentos()+torre2.get_numero_movimentos()+torre3.get_numero_movimentos()))
     print('\t\t\t   Torre 1\t\t\t\tTorre 2\t\t\t\t Torre 3')
     str_todos_discos = ''
     size_torres = torre1.get_tamanho()
@@ -14,61 +23,90 @@ def print_torres():
         numbers_inverso = []
         for item in range(qtd_max_discos(),0-1,-1):
             numbers_inverso.append(item)
-
-
         bar = ' ' * numbers_inverso[qtd] + '▄' * qtd + '║' + '▄' * qtd + " "*numbers_inverso[qtd]
         return bar
 
     for linha in range(size_torres-1,0-1,-1):
-        #print('\t '+str(torre1.get_discos()[linha]) + "\t\t\t  " + str(torre2.get_discos()[linha]) + "\t\t\t  " + str(torre3.get_discos()[linha]))
         print('        ' + built_bar_center(torre1.get_discos()[linha])+ built_bar_center(torre2.get_discos()[linha]) + built_bar_center(torre3.get_discos()[linha]))
 
-    print(str_todos_discos)
 
-if __name__ == '__main__':
-    print("╠╠╠ Jogo Torre de Hanói ╣╣╣")
+def inicializa_jogo():
+    print("\n╠╠╠ Jogo Torre de Hanói ╣╣╣")
     print("Desenvolvido por Michel Galvão como Atividade Avaliativa da matéria Programação Orientada à objetos em Python da graduação de Engenharia de Software\n")
-    qtd_discos = 0
-    while qtd_discos <3 or qtd_discos >qtd_max_discos():
-        qtd_discos = int(input("Informe a quantidade de discos desejados para o jogo (3 a "+str(qtd_max_discos())+"): "))
+    qtd_discos = input("Informe a quantidade de discos desejados para o jogo (3 a " + str(qtd_max_discos()) + "): ")
+    while qtd_discos == '' or int(qtd_discos) < 3 or int(qtd_discos) > qtd_max_discos():
+        qtd_discos = input("Entrada inválida. Informe novamente: ")
+    qtd_discos = int(qtd_discos)
+    print("Você consegue vencer o jogo com " + str(numero_tentativas_min(qtd_discos)) + " tentativas.")
+
     torre1 = Torre(qtd_discos)
     torre2 = Torre(qtd_discos)
     torre3 = Torre(qtd_discos)
 
-    for peso in range(qtd_discos,0,-1):
+    for peso in range(qtd_discos, 0, -1):
         torre1.desempilha_disco()
 
-    for peso in range(1,qtd_discos+1):
-        torre1.empilha_disco(peso)
+    for peso in range(1, qtd_discos + 1):
+        torre1.inicia_torre_com_disco(peso)
+    return torre1, torre2, torre3 # Retorna uma tupla
+
+if __name__ == '__main__':
+    torre1, torre2, torre3 = inicializa_jogo() #Atribuir tupla retornada
 
     while True:
         print_torres()
         de_valido = True
         disco = 0
-        de = int(input("Digite 1, 2 ou 3 para informar a identificação da torre de origem para mover o disco: "))
+
+        entrada = input("Digite 1, 2 ou 3 para informar a identificação da torre de origem para mover o disco: ")
+        while entrada == '' or int(entrada) >3 or int(entrada) <1:
+            entrada = input("Entrada inválida. Informe novamente: ")
+
+        de = int(entrada)
+        torre_origem = -1
         if de == 1:
             disco = torre1.get_first()
-            torre1.set_vazio_first_position_torre()
+            torre_origem = torre1
         elif de == 2:
             disco = torre2.get_first()
-            torre2.set_vazio_first_position_torre()
+            torre_origem = torre2
         elif de == 3:
             disco = torre3.get_first()
-            torre3.set_vazio_first_position_torre()
+            torre_origem = torre3
         else:
             de_valido = False
             print("Identificação de torre originária inválida. Tente novamente.")
 
         if de_valido == True:
-            para = int(input("Digite 1, 2 ou 3 para informar a identificação da torre de destino para mover o disco: "))
+            entrada = input("Digite 1, 2 ou 3 para informar a identificação da torre de destino para mover o disco: ")
+            while entrada == '' or int(entrada) > 3 or int(entrada) < 1:
+                entrada = input("Entrada inválida. Informe novamente: ")
+
+            para = int(entrada)
             if para == 1:
-                torre1.insert(disco)
+                if torre1.insert(disco) == True:
+                    torre_origem.set_vazio_first_position_torre()
             elif para == 2:
-                torre2.insert(disco)
+                if torre2.insert(disco) == True:
+                    torre_origem.set_vazio_first_position_torre()
             elif para == 3:
-                torre3.insert(disco)
+                if torre3.insert(disco) == True:
+                    torre_origem.set_vazio_first_position_torre()
             else:
                 print("Identificação de torre destinatária inválida. Tente novamente.")
+
+
+            if torre3.get_discos()[torre3.get_tamanho()-1] == 1:
+                print_torres()
+                print("Você venceu o jogo com "+str(numero_tentativas())+" tentativas! Sua pontuação percentual: "+str(calcular_pontuacao(numero_tentativas_min(torre1.get_tamanho()),numero_tentativas()))+"%")
+                entrada = input("Deseja jogar novamente? Digite 1 para sim ou 0 para não: ")
+                while entrada == '' or int(entrada) < 0 or int(entrada) > 1:
+                    entrada = input("Entrada inválida. Informe novamente: ")
+                entrada = int(entrada)
+                if entrada == 1:
+                    torre1, torre2, torre3 = inicializa_jogo()
+                elif entrada == 0:
+                    break
 
 
 
